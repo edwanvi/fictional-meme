a85e8c2075661eb9aa801e195c5d6a30ab991737
Edwan Vi <tkdberger@gmail.com>
2016-11-15 16:20:00 -0500

    Set up event handling
    I think after this i can start feature branches

--- src/main/java/me/itstheholyblack/testmodpleaseignore/mod.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/mod.java
@@ -3,13 +3,25 @@ package me.itstheholyblack.testmodpleaseignore;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MODID, version = Reference.VERSION)
 public class mod {
+	@Instance
+    public static mod instance = new mod();
+
     @EventHandler
-    public void init(FMLInitializationEvent event) {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    public void preInit(FMLPreInitializationEvent e) {
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
     }
 }
