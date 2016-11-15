832a16df3b57a8bba655f70cbdd39303d798b9e8
Edwan Vi <tkdberger@gmail.com>
2016-11-15 15:04:03 -0500

    Create reference class

---
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/Reference.java
@@ -0,0 +1,7 @@
+package me.itstheholyblack.testmodpleaseignore;
+
+public class Reference {
+	public static final String VERSION = "0.0.1-gamma";
+	public static final String MODID = "TestModPleaseIgnore";
+	public static final String ACCPETED_MINECRAFT_VERSION = "[1.10.2]";
+}
