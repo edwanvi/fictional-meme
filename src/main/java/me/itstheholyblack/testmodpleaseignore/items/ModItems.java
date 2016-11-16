727c94a00439bd9b25a87cd2f4e7843096b30b53
Edwan Vi <tkdberger@gmail.com>
2016-11-16 10:27:43 -0500

    git is spooky

---
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/ModItems.java
@@ -0,0 +1,12 @@
+package me.itstheholyblack.testmodpleaseignore.items;
+
+import net.minecraft.item.Item;
+
+public final class ModItems {
+	
+	public static Item testingItem;
+	
+	public static void createItems() {
+		
+	}
+}
