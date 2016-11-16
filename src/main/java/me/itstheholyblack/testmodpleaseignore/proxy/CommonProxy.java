102822cc91fdec3dd7369b02445d284eafd1c84c
Edwan Vi <tkdberger@gmail.com>
2016-11-16 10:30:43 -0500

    Call createItems in CommonProxy

--- src/main/java/me/itstheholyblack/testmodpleaseignore/proxy/CommonProxy.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/proxy/CommonProxy.java
@@ -1,12 +1,13 @@
 package me.itstheholyblack.testmodpleaseignore.proxy;
 
+import me.itstheholyblack.testmodpleaseignore.items.ModItems;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent e) {
-
+		ModItems.createItems();
     }
 
     public void init(FMLInitializationEvent e) {
