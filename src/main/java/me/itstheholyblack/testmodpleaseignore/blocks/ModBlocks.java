01c34db3a70515c22fdda7133f36791604092a06
Edwan Vi <tkdberger@gmail.com>
2016-11-16 18:13:02 -0500

    the ground has been broken

---
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/ModBlocks.java
@@ -0,0 +1,10 @@
+package me.itstheholyblack.testmodpleaseignore.blocks;
+
+import net.minecraft.block.Block;
+
+public final class ModBlocks {
+	public static Block tutorialBlock;
+	public static void createBlocks() {
+		
+	}
+}
--- src/main/java/me/itstheholyblack/testmodpleaseignore/proxy/CommonProxy.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/proxy/CommonProxy.java
@@ -1,5 +1,6 @@
 package me.itstheholyblack.testmodpleaseignore.proxy;
 
+import me.itstheholyblack.testmodpleaseignore.blocks.ModBlocks;
 import me.itstheholyblack.testmodpleaseignore.items.ModItems;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -8,6 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent e) {
 		ModItems.createItems();
+		ModBlocks.createBlocks();
     }
 
     public void init(FMLInitializationEvent e) {
