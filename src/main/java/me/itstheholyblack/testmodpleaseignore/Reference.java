4701abb33d058e9b6da73aaae97a39794c756d81
Edwan Vi <tkdberger@gmail.com>
2016-11-16 15:19:01 -0500

    lowercase mod id for 1.11

--- src/main/java/me/itstheholyblack/testmodpleaseignore/Reference.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/Reference.java
@@ -2,7 +2,7 @@ package me.itstheholyblack.testmodpleaseignore;
 
 public class Reference {
 	public static final String VERSION = "0.0.1-gamma";
-	public static final String MODID = "TestModPleaseIgnore";
+	public static final String MODID = "testmodpleaseignore";
 	public static final String ACCPETED_MINECRAFT_VERSION = "[1.10.2]";
 	public static final String MODNAME = "Test Mod Please Ignore";
 }
