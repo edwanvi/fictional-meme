6f68bc3d3396c174c1404d095ab21c842ad2971d
Edwan Vi <tkdberger@gmail.com>
2016-11-16 10:46:02 -0500

    Use basic item code by McJty

--- src/main/java/me/itstheholyblack/testmodpleaseignore/items/ModItems.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/ModItems.java
@@ -1,12 +1,13 @@
 package me.itstheholyblack.testmodpleaseignore.items;
 
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public final class ModItems {
 	
-	public static Item testingItem;
+	public static BasicItem testingItem;
 	
 	public static void createItems() {
-		
+		testingItem = new BasicItem();
 	}
 }
