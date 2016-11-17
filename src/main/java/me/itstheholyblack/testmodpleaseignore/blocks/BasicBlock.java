6523933797aa1100dadcbdbf0646f5269fac24d7
Edwan Vi <tkdberger@gmail.com>
2016-11-17 10:51:34 -0500

    git you son of a b*tch

--- src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/BasicBlock.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/BasicBlock.java
@@ -3,14 +3,19 @@ package me.itstheholyblack.testmodpleaseignore.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class BasicBlock extends Block {
 	public BasicBlock(String unlocalizedName, Material material, float hardness, float resistance) {
         super(material);
-        this.setUnlocalizedName(unlocalizedName);
-        this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-        this.setHardness(hardness);
-        this.setResistance(resistance);
+        setUnlocalizedName(unlocalizedName);
+        setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        setHardness(hardness);
+        setResistance(resistance);
+        setRegistryName(unlocalizedName);
+        GameRegistry.register(this);
+        GameRegistry.register(new ItemBlock(this), getRegistryName());
     }
 
     public BasicBlock(String unlocalizedName, float hardness, float resistance) {
