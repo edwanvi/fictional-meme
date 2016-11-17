2705a7293e29b3d0a66b3d955f0c7c1f4da5dfd4
Edwan Vi <tkdberger@gmail.com>
2016-11-16 19:36:38 -0500

    your logs just got ten feet longer

--- src/main/java/me/itstheholyblack/testmodpleaseignore/proxy/CommonProxy.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/proxy/CommonProxy.java
@@ -8,7 +8,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent e) {
+		System.out.println("Creating items...");
 		ModItems.createItems();
+		System.out.println("Creating blocks...");
 		ModBlocks.createBlocks();
     }
 
