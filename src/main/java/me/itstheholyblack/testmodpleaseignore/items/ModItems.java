1b624cee7f912eb55b2e12d83cabaf7a5d5f4750
Edwan Vi <tkdberger@gmail.com>
2016-11-17 15:35:51 -0500

    new item

--- src/main/java/me/itstheholyblack/testmodpleaseignore/items/ModItems.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/ModItems.java
@@ -8,13 +8,17 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public final class ModItems {
 	
 	public static AutoItem testingItem;
+	public static AutoItem witheredWater;
 	
 	public static void createItems() {
 		testingItem = new AutoItem("firstitem");
+		witheredWater = new AutoItem("witheredwater");
 	}
 	@SideOnly(Side.CLIENT)
     public static void initModels() {
 		System.out.println("Loading model for item firstitem");
         testingItem.initModel();
+        System.out.println("Loading model for item witheredwater");
+        witheredWater.initModel();
     }
 }
