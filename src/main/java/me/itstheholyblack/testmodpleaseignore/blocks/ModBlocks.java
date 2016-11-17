52ac361bb4f970cbae71dace31444e3c6506e9f8
Edwan Vi <tkdberger@gmail.com>
2016-11-17 12:20:01 -0500

    First untextured block

--- src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/ModBlocks.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/ModBlocks.java
@@ -3,8 +3,8 @@ package me.itstheholyblack.testmodpleaseignore.blocks;
 import net.minecraft.block.Block;
 
 public final class ModBlocks {
-	public static Block tutorialBlock;
+	public static BasicBlock tutorialBlock;
 	public static void createBlocks() {
-		
+		tutorialBlock = new BasicBlock("firstblock");
 	}
 }
