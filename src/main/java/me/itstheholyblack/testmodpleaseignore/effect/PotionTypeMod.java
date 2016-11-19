3ee2b30edc08256e981a47d076b6cc9290d7867f
Edwan Vi <tkdberger@gmail.com>
2016-11-19 07:43:56 -0500

    Whitespace and Javadocs

--- src/main/java/me/itstheholyblack/testmodpleaseignore/effect/PotionTypeMod.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/effect/PotionTypeMod.java
@@ -7,9 +7,12 @@ import net.minecraft.potion.PotionType;
 import net.minecraft.util.ResourceLocation;
 
 /**
+ * Class to make new potion types, NOT new effects
  * @author wiresegal
+ * @param name The unlocalized name of the potion
+ * @param potionEffects The PotionEffect this potion gives
  * */
-public class PotionTypeMod  extends PotionType {
+public class PotionTypeMod extends PotionType {
 	public PotionTypeMod(String name, PotionEffect... potionEffects) {
 		super(name, potionEffects);
         GameRegistry.register(this, new ResourceLocation(Reference.MODID, name));
