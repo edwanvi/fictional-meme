7ecf852b28e5569b8976ff8854be1cd712889fd8
Edwan Vi <tkdberger@gmail.com>
2016-11-19 16:40:12 -0500

    technically i'm done here.
    *technically*

--- src/main/java/me/itstheholyblack/testmodpleaseignore/items/armor/LeapingBoots.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/armor/LeapingBoots.java
@@ -1,15 +1,19 @@
 package me.itstheholyblack.testmodpleaseignore.items.armor;
 
+import me.itstheholyblack.testmodpleaseignore.Reference;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class LeapingBoots extends ItemArmor {
 
 	public LeapingBoots(EntityEquipmentSlot type, String name) {
 		this(type, name, ArmorTypes.ENDER_CLOTH_MAT);
-		// TODO Auto-generated constructor stub
 	}
 	public LeapingBoots(EntityEquipmentSlot type, String name, ArmorMaterial mat){
 		super(mat, 0, type);
+		setUnlocalizedName(Reference.MODID + "." + "leapingboots");
+		setRegistryName("leapingboots");
+		GameRegistry.register(this);
 	}
 }
