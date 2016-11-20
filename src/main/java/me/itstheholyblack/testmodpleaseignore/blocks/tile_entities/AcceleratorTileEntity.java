11ef70a98366adb7442a433aef2f7544613068c2
Edwan Vi <tkdberger@gmail.com>
2016-11-20 15:03:58 -0500

    gtg

--- src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/tile_entities/AcceleratorTileEntity.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/tile_entities/AcceleratorTileEntity.java
@@ -2,6 +2,7 @@ package me.itstheholyblack.testmodpleaseignore.blocks.tile_entities;
 
 import javax.annotation.Nullable;
 
+import me.itstheholyblack.testmodpleaseignore.blocks.ModBlocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
@@ -11,25 +12,22 @@ import net.minecraft.util.math.BlockPos;
 
 public class AcceleratorTileEntity extends TileEntity implements ITickable {
 	private int count;
-	private boolean ruined = false;
+	private boolean safe;
 	private BlockPos pos = getPos();
 	@Override
 	public void update() {
+		// safe = test_all_faces();
 		//if (worldObj.isRemote) {
-		if(count > 100 && !ruined) {
+		if(count > 100 && !safe) {
 			this.worldObj.newExplosion(null, getPos().getX(), getPos().getY(), getPos().getZ(), 4.0F, true, true);
-			ruined = true;
 			markDirty();
-		} else if (ruined) {
-			worldObj.setBlockState(pos, Blocks.AIR.getDefaultState());
-			this.invalidate();
 		} else {
-			increase();
+			safe = test_all_faces();
+			markDirty();
 		}
 	}
 	public int increase() {
 		count++;
-		// tell minecraft this data needs to be saved
 		markDirty();
 		return count;
 	}
@@ -50,5 +48,23 @@ public class AcceleratorTileEntity extends TileEntity implements ITickable {
         super.writeToNBT(compound);
         compound.setInteger("counter", count);
         return compound;
+    }
+    public boolean test_all_faces() {
+    	boolean pos_x_safe = false;
+    	boolean neg_x_safe = false;
+    	if (worldObj.getBlockState(pos.add(1, 0, 0)).getBlock() == ModBlocks.tutorialBlock) {
+    		pos_x_safe = true;
+    	}
+    	if (worldObj.getBlockState(pos.add(-1, 0, 0)).getBlock() == ModBlocks.tutorialBlock) {
+    		neg_x_safe = true;
+    	}
+		if (pos_x_safe && neg_x_safe) {
+			safe = true;
+			markDirty();
+		} else {
+			safe = false;
+			markDirty();
+		}
+		return safe;
     }
 }
