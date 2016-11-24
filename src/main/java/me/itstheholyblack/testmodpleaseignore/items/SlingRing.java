79754c03136896406ba7704478281bf798c6489c
Edwan Vi <tkdberger@gmail.com>
2016-11-24 16:37:24 -0500

    New iem: the sling ring

---
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/SlingRing.java
@@ -0,0 +1,15 @@
+package me.itstheholyblack.testmodpleaseignore.items;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+public class SlingRing extends Item {
+	// @Override
+	/* public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		
+	} */
+}
