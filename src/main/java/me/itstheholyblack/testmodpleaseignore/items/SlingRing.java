aec01bfb3a80a9a81ba92995261e228bed4e3e67
Edwan Vi <tkdberger@gmail.com>
2016-11-25 17:59:22 -0500

    Cleanup, tooltip from NBT

--- src/main/java/me/itstheholyblack/testmodpleaseignore/items/SlingRing.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/SlingRing.java
@@ -1,6 +1,10 @@
 package me.itstheholyblack.testmodpleaseignore.items;
 
+import java.util.List;
+
 import me.itstheholyblack.testmodpleaseignore.Reference;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -12,10 +16,13 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class SlingRing extends Item {
-	// @Override
+	NBTTagCompound compound;
 	public SlingRing() {
 		this.maxStackSize = 1;
 		setRegistryName("sling_ring");
@@ -25,7 +32,7 @@ public class SlingRing extends Item {
 	}
 	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand) {
 		// if no NBT is saved, make some
-		if (stack.getTagCompound() == null) {
+		if (stack.getTagCompound() == null | playerIn.isSneaking()) {
 			stack.setTagCompound(new NBTTagCompound());
 			NBTInit(stack, playerIn);
 			System.out.println(playerIn.getName() + " initialized a sling ring. \\[T]/");
@@ -41,5 +48,28 @@ public class SlingRing extends Item {
 		compound.setInteger("x", player.getPosition().getX());
 		compound.setInteger("y", player.getPosition().getY());
 		compound.setInteger("z", player.getPosition().getZ());
+	}
+	public void initModel() {
+		ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+	}
+	@SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+		NBTTagCompound compound = stack.getTagCompound();
+		if (compound != null) {
+			int x = compound.getInteger("x");
+			int y = compound.getInteger("y");
+			int z = compound.getInteger("z");
+			// (0, 0, 0) isn't reachable without breaking bedrock
+			if (x == 0 && y == 0 && z == 0) {
+				tooltip.add(I18n.format("mouseovertext.sling_ring"));
+			} else {
+				String fulltip = I18n.format("mouseovertext.sling_ring")+"\nX: " + Integer.toString(x) + "\nY: " + Integer.toString(y) + "\nZ: " + Integer.toString(z);
+				tooltip.add(fulltip);
+			}
+		} else {
+			tooltip.add(I18n.format("mouseovertext.sling_ring"));
+		}
+        super.addInformation(stack, playerIn, tooltip, advanced);
 	}
 }
