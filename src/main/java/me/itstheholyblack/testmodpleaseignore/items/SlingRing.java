f338516b2cd901e160108c25dcd5987aebb6f56f
Edwan Vi <tkdberger@gmail.com>
2016-11-24 22:03:42 -0500

    Basic NBT data work

--- src/main/java/me/itstheholyblack/testmodpleaseignore/items/SlingRing.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/SlingRing.java
@@ -3,13 +3,26 @@ package me.itstheholyblack.testmodpleaseignore.items;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
 public class SlingRing extends Item {
 	// @Override
-	/* public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand) {
-		
-	} */
+	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		// if no NBT is saved, make some
+		if (stack.getTagCompound() == null) {
+			stack.setTagCompound(new NBTTagCompound());
+			NBTInit(stack);
+		}
+		return new ActionResult<>(EnumActionResult.SUCCESS, stack);
+	}
+	public void NBTInit(ItemStack stack) {
+		NBTTagCompound compound = stack.getTagCompound();
+		compound.setInteger("x", 0);
+		compound.setInteger("y", 0);
+		compound.setInteger("z", 0);
+	}
 }
