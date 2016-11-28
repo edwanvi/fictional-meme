c333b3640a06a32ddee6012bb00252527828ed10
Edwan Vi <tkdberger@gmail.com>
2016-11-28 10:58:13 -0500

    gotcha

--- src/main/java/me/itstheholyblack/testmodpleaseignore/items/ItemTestCloak.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/ItemTestCloak.java
@@ -8,15 +8,20 @@ import me.itstheholyblack.testmodpleaseignore.model.ModelCloak;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemTestCloak extends Item implements IBauble, IRenderBauble {
+	
+	private static final ResourceLocation texture = new ResourceLocation("testmodpleaseignore:textures/models/idunnCloak.png");
 	
 	@SideOnly(Side.CLIENT)
 	private static ModelCloak model;
@@ -34,7 +39,12 @@ public class ItemTestCloak extends Item implements IBauble, IRenderBauble {
 	public BaubleType getBaubleType(ItemStack itemstack) {
 		return BaubleType.BODY;
 	}
-
+	
+	@SideOnly(Side.CLIENT)
+	ResourceLocation getCloakTexture() {
+		return texture;
+	}
+	
 	@Override
 	public void onPlayerBaubleRender(ItemStack stack, EntityPlayer player, RenderType type, float partialTicks) {
 		if(type == RenderType.BODY) {
@@ -53,12 +63,16 @@ public class ItemTestCloak extends Item implements IBauble, IRenderBauble {
 			Minecraft.getMinecraft().renderEngine.bindTexture(getCloakTexture());
 			model.render(1F);
 
-			int light = 15728880;
+			/* int light = 15728880;
 			int lightmapX = light % 65536;
 			int lightmapY = light / 65536;
 			OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightmapX, lightmapY);
 			Minecraft.getMinecraft().renderEngine.bindTexture(getCloakGlowTexture());
-			model.render(1F);
+			model.render(1F); */ 
 		}
 	}
+	@SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
 }
