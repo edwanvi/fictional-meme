a75fb20d21b6306bbe27eedcfbd03082aafd1d45
Edwan Vi <tkdberger@gmail.com>
2016-11-29 20:39:26 -0500

    I swear to god if you don't fix this me I will harm you

--- src/main/java/me/itstheholyblack/testmodpleaseignore/EventHandles.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/EventHandles.java
@@ -1,5 +1,10 @@
 package me.itstheholyblack.testmodpleaseignore;
 
-public class EventHandles {
+import baubles.api.BaublesApi;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 
+public class EventHandles {
+	IInventory baublesInv = BaublesApi.getBaubles(null);
+	ItemStack amuletStack = baublesInv.getStackInSlot(0);
 }
