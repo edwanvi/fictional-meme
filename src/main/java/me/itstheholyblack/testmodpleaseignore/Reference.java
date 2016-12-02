345fb6d57f429a1a7b4d0ba633c5b1c867b552c9
Edwan Vi <tkdberger@gmail.com>
2016-12-02 16:52:09 -0500

    Version increment
    God mcmod.info was behind ._.

--- src/main/java/me/itstheholyblack/testmodpleaseignore/Reference.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/Reference.java
@@ -1,7 +1,7 @@
 package me.itstheholyblack.testmodpleaseignore;
 
 public class Reference {
-	public static final String VERSION = "0.0.3-gamma";
+	public static final String VERSION = "0.0.4-gamma";
 	public static final String MODID = "testmodpleaseignore";
 	public static final String ACCPETED_MINECRAFT_VERSION = "[1.10.2]";
 	public static final String MODNAME = "Test Mod Please Ignore";
