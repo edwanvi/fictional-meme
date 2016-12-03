810ee4b0c266071dacde0f9947e9661e67845ca9
Edwan Vi <tkdberger@gmail.com>
2016-12-02 20:35:23 -0500

    Move away from the deprecated method

--- src/main/java/me/itstheholyblack/testmodpleaseignore/EventHandles.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/EventHandles.java
@@ -1,9 +1,9 @@
 package me.itstheholyblack.testmodpleaseignore;
 
 import baubles.api.BaublesApi;
+import baubles.api.cap.IBaublesItemHandler;
 import me.itstheholyblack.testmodpleaseignore.items.ItemTestCloak;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -15,7 +15,7 @@ public class EventHandles {
 		// only do this if the event is on a player
 		if (event.getEntityLiving() instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-			IInventory baublesInv = BaublesApi.getBaubles(player);
+			IBaublesItemHandler baublesInv = BaublesApi.getBaublesHandler(player);
 			ItemStack cloakStack = baublesInv.getStackInSlot(5);
 			if (cloakStack != null && cloakStack.getItem() instanceof ItemTestCloak) {
 				// allow flight
