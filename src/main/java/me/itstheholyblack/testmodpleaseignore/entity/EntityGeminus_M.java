e39f7a72c1d29ce10a93dc01cd4cb8f445164b40
Edwan Vi <tkdberger@gmail.com>
2016-12-05 19:30:08 -0500

    It begins.

---
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityGeminus_M.java
@@ -0,0 +1,12 @@
+package me.itstheholyblack.testmodpleaseignore.entity;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.world.World;
+
+public class EntityGeminus_M extends EntityLiving {
+
+	public EntityGeminus_M(World worldIn) {
+		super(worldIn);
+		// TODO Auto-generated constructor stub
+	}
+}
