2496ffbc00a102ddb7cfc964ba2c2404f41e661a
Edwan Vi <tkdberger@gmail.com>
2016-12-06 10:09:59 -0500

    Register entities

---
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/ModEntities.java
@@ -0,0 +1,17 @@
+package me.itstheholyblack.testmodpleaseignore.entity;
+
+import me.itstheholyblack.testmodpleaseignore.mod;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+/**
+ * Class to handle registration of custom entities.
+ * @author McJty
+ * @author Edwan Vi
+ */
+public class ModEntities {
+	public static void init() {
+		// Every entity in our mod has an ID (local to this mod)
+		int id = 1;
+		EntityRegistry.registerModEntity(EntityMissile.class, "Missile", id++, mod.instance, 64, 3, true, 0x996600, 0x00ff00);
+	}		
+}
