b8b38f914e9850fa72d4ef857351e3eb7a4ea28f
Edwan Vi <tkdberger@gmail.com>
2016-12-07 15:25:08 -0500

    You'd think I would have caught this one sooner.
    Die bugs die

--- src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityShulkerMinion.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityShulkerMinion.java
@@ -24,6 +24,6 @@ public class EntityShulkerMinion extends EntityShulker {
 	public void onDeath(@Nonnull DamageSource source) {
 		super.onDeath(source);
 		if (boss != null)
-			boss.shulkerList.remove(this);
+			boss.shulkerList.remove(this.getUniqueID());
 	}
 }
