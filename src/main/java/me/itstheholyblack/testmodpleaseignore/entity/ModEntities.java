87a05fc5c05d3d8ddee2a89a71fdcd79863e1254
Edwan Vi <tkdberger@gmail.com>
2016-12-08 09:52:18 -0500

    basic render
    TODO use actual skin

--- src/main/java/me/itstheholyblack/testmodpleaseignore/entity/ModEntities.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/ModEntities.java
@@ -1,7 +1,11 @@
 package me.itstheholyblack.testmodpleaseignore.entity;
 
 import me.itstheholyblack.testmodpleaseignore.mod;
+import me.itstheholyblack.testmodpleaseignore.entity.render.RenderGeminus_M;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Class to handle registration of custom entities.
@@ -15,5 +19,9 @@ public class ModEntities {
 		EntityRegistry.registerModEntity(EntityMissile.class, "Missile", id++, mod.instance, 64, 3, true, 0x996600, 0x00ff00);
 		EntityRegistry.registerModEntity(EntityGeminus_M.class, "entityGeminus_M", id++, mod.instance, 100, 6, true, 0x996600, 0x00ff00);
 		EntityRegistry.registerModEntity(EntityShulkerMinion.class, "shulkerMinion", id++, mod.instance, 64, 3, true);
-	}		
+	}
+	@SideOnly(Side.CLIENT)
+    public static void initModels() {
+        RenderingRegistry.registerEntityRenderingHandler(EntityGeminus_M.class, RenderGeminus_M.FACTORY);
+    }
 }
--- src/main/java/me/itstheholyblack/testmodpleaseignore/entity/render/RenderGeminus_M.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/render/RenderGeminus_M.java
@@ -3,18 +3,28 @@ package me.itstheholyblack.testmodpleaseignore.entity.render;
 import me.itstheholyblack.testmodpleaseignore.entity.EntityGeminus_M;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
 
 public class RenderGeminus_M extends RenderLiving<EntityGeminus_M> {
-	public RenderGeminus_M(RenderManager rendermanagerIn, ModelBase modelbaseIn, float shadowsizeIn) {
-		super(rendermanagerIn, new ModelPlayer(shadowsizeIn, false), shadowsizeIn);
+	public static final Factory FACTORY = new Factory();
+	private ResourceLocation mobTexture = new ResourceLocation("testmodpleaseignore:textures/entity/geminus_m.png");
+	public RenderGeminus_M(RenderManager rendermanagerIn) {
+		super(rendermanagerIn, new ModelPlayer(0.5F, false), 0.5F);
 		// TODO Auto-generated constructor stub
 	}
 	@Override
 	protected ResourceLocation getEntityTexture(EntityGeminus_M entity) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	public static class Factory implements IRenderFactory<EntityGeminus_M> {
+        @Override
+        public Render<? super EntityGeminus_M> createRenderFor(RenderManager manager) {
+            return new RenderGeminus_M(manager);
+        }
 	}
 }
