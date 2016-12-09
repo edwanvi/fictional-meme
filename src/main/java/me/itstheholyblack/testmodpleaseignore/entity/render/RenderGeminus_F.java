3a5d54d3df6c05d94256e8637592280dfc489d62
Edwan Vi <tkdberger@gmail.com>
2016-12-09 09:56:58 -0500

    render

--- src/main/java/me/itstheholyblack/testmodpleaseignore/entity/render/RenderGeminus_F.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/render/RenderGeminus_F.java
@@ -14,16 +14,16 @@ public class RenderGeminus_F extends RenderLiving<EntityGeminus_F> {
 	public static final Factory FACTORY = new Factory();
 	private ResourceLocation mobTexture = new ResourceLocation("testmodpleaseignore:textures/entity/geminus_f.png");
 	public RenderGeminus_F(RenderManager rendermanagerIn) {
-		super(rendermanagerIn, new ModelPlayer(0.5F, false), 0.5F);
+		super(rendermanagerIn, new ModelPlayer(0.5F, true), 0.5F);
 	}
 	@Override
 	protected ResourceLocation getEntityTexture(EntityGeminus_F entity) {
 		return mobTexture;
 	}
-	public static class Factory implements IRenderFactory<EntityGeminus_M> {
+	public static class Factory implements IRenderFactory<EntityGeminus_F> {
         @Override
-        public Render<? super EntityGeminus_M> createRenderFor(RenderManager manager) {
-            return new RenderGeminus_M(manager);
+        public Render<? super EntityGeminus_F> createRenderFor(RenderManager manager) {
+            return new RenderGeminus_F(manager);
         }
 	}
 }
