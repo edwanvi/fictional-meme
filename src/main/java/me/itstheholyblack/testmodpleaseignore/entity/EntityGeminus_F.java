f81856a92a411ad48f30222ba502b7263000ddca
Edwan Vi <tkdberger@gmail.com>
2016-12-09 11:55:04 -0500

    Teleports

--- src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityGeminus_F.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityGeminus_F.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
@@ -30,7 +35,12 @@ public class EntityGeminus_F extends EntityLiving {
 	// boss bar
 	private final BossInfoServer bossInfo = (BossInfoServer)(new BossInfoServer(this.getDisplayName(), BossInfo.Color.GREEN, BossInfo.Overlay.NOTCHED_20));
 	private static final DataParameter<BlockPos> HOME = EntityDataManager.createKey(EntityGeminus_M.class, DataSerializers.BLOCK_POS);
-	public EntityGeminus_M brother;
+	// brother geminus, may be null
+	@Nullable public EntityGeminus_M brother;
+	// closest player, may be null
+	@Nullable private EntityPlayer closestPlayer;
+	private static final double TELEPORT_RANGE_DOUBLE = 32.0D;
+	private static final int TELEPORT_RANGE_INT = (int) TELEPORT_RANGE_DOUBLE;
 	public EntityGeminus_F(World worldIn) {
 		super(worldIn);
 		this.brother = null;
@@ -51,7 +61,18 @@ public class EntityGeminus_F extends EntityLiving {
 		dataManager.register(SPAWNING, false);
 		dataManager.register(HOME, BlockPos.ORIGIN);
 	}
+	protected void initEntityAI() {
+		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.applyEntityAI();
+	}
+	protected void applyEntityAI() {
+		// no-op
+	}
 	public void onLivingUpdate() {
+		if (this.getHome().equals(BlockPos.ORIGIN)) {
+			// i very much doubt it's at 0 0 0
+			this.setHome(this.getPosition());
+		}
 		if (this.playersWhoAttacked == null && brother != null) {
 			// first tick
 			this.playersWhoAttacked = this.brother.playersWhoAttacked;
@@ -60,8 +81,35 @@ public class EntityGeminus_F extends EntityLiving {
 		} else {
 			this.playersWhoAttacked = new ArrayList<>();
 		}
+		this.limbSwingAmount = 0.0F;
+		boolean spawning = dataManager.get(SPAWNING);
+		this.closestPlayer = this.worldObj.getClosestPlayerToEntity(this, 8.0D);
+		float PERCENT_HP = this.getHealth() / this.getMaxHealth();
+		if (this.closestPlayer != null && this.closestPlayer.isSpectator()) {
+            this.closestPlayer = null;
+        }
+		this.bossInfo.setPercent(this.getHealth() / this.getMaxHealth());
 		super.onLivingUpdate();
 	}
+	protected void updateAITasks() {
+		if (this.closestPlayer != null && this.closestPlayer.getDistanceSqToEntity(this) < 1.5D) {
+			this.teleportRandomly();
+		}
+		BlockPos homePos = getHome();
+		// teleport home
+		if (this.getDistance(homePos.getX(), homePos.getY(), homePos.getZ()) > 64 && !homePos.equals(BlockPos.ORIGIN)) {
+			this.setPositionAndUpdate(homePos.getX(), homePos.getY(), homePos.getZ());
+		}
+		super.updateAITasks();
+	}
+	// setters and getters below this line *only*
+	public void setHome(BlockPos pos) {
+		dataManager.set(HOME, pos);
+	}
+	public BlockPos getHome() {
+		return dataManager.get(HOME);
+	}
+	// setters and getters above this line *only*
 	// +++BEGIN ENTITYWITHER CODE+++
     /**
      * Add the given player to the list of players tracking this entity. For instance, a player may track a boss in
@@ -86,4 +134,36 @@ public class EntityGeminus_F extends EntityLiving {
         return false;
     }
     // +++END ENTITYWITHER CODE+++
+    // ===BEGIN ENTITYENDERMAN CODE===
+ 	/**
+ 	 * Teleport to a random position within 64 blocks.
+ 	 * @return A boolean indicating whether or not we succeeded.
+ 	 * @author Notch
+ 	 */
+ 	private boolean teleportRandomly() {
+ 		double d0 = posX + (rand.nextDouble() - 0.5D) * TELEPORT_RANGE_DOUBLE;
+ 		double d1 = posY + (rand.nextInt(TELEPORT_RANGE_INT) - 32);
+ 		double d2 = posZ + (rand.nextDouble() - 0.5D) * TELEPORT_RANGE_DOUBLE;
+ 		return teleportTo(d0, d1, d2);
+ 	}
+ 	/**
+ 	 * Teleport to a given position.
+ 	 * @param x The x coordinate of our destination.
+ 	 * @param y The y coordinate of our destination.
+ 	 * @param z The z coordinate of our destination.
+ 	 * @return A boolean indicating whether or not we succeeded.
+ 	 * @author Notch
+ 	 */
+ 	private boolean teleportTo(double x, double y, double z) {
+         net.minecraftforge.event.entity.living.EnderTeleportEvent event = new net.minecraftforge.event.entity.living.EnderTeleportEvent(this, x, y, z, 0);
+         if (net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(event)) return false;
+         boolean flag = this.attemptTeleport(event.getTargetX(), event.getTargetY(), event.getTargetZ());
+
+         if (flag) {
+             this.worldObj.playSound((EntityPlayer)null, this.prevPosX, this.prevPosY, this.prevPosZ, SoundEvents.ENTITY_ENDERMEN_TELEPORT, this.getSoundCategory(), 1.0F, 1.0F);
+             this.playSound(SoundEvents.ENTITY_ENDERMEN_TELEPORT, 1.0F, 1.0F);
+         }
+         return flag;
+     }
+     // ===END ENTITYENDERMAN CODE===
 }
