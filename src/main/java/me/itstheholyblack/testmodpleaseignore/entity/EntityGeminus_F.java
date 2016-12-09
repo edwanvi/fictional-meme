1c47e070349ef0c3da2437dc741edbe406d0acfc
Edwan Vi <tkdberger@gmail.com>
2016-12-09 10:09:20 -0500

    Basic stuff

--- src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityGeminus_F.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityGeminus_F.java
@@ -1,12 +1,20 @@
 package me.itstheholyblack.testmodpleaseignore.entity;
 
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.world.BossInfo;
+import net.minecraft.world.BossInfoServer;
 import net.minecraft.world.World;
 
 public class EntityGeminus_F extends EntityLiving {
-
+	// boss bar
+	private final BossInfoServer bossInfo = (BossInfoServer)(new BossInfoServer(this.getDisplayName(), BossInfo.Color.GREEN, BossInfo.Overlay.NOTCHED_20));
+	public EntityGeminus_M brother;
 	public EntityGeminus_F(World worldIn) {
 		super(worldIn);
-		// TODO Auto-generated constructor stub
+		this.brother = null;
+	}
+	public EntityGeminus_F(World worldIn, EntityGeminus_M bro) {
+		this(worldIn);
+		this.brother = bro;
 	}
 }
