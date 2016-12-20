45e2c85ff42c4d90849b37eda59e2f976134b409
Edwan Vi <tkdberger@gmail.com>
2016-12-20 10:26:42 -0500

    Improve Javadoc

--- src/main/java/me/itstheholyblack/testmodpleaseignore/core/ParticleEffects.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/core/ParticleEffects.java
@@ -10,17 +10,17 @@ public class ParticleEffects {
 	 * Spawns some particles server side.
 	 * 
 	 * @param worldIn
-	 *            - the world to spawn the particles in.
+	 *            The world to spawn the particles in.
 	 * @param X
-	 *            - X coordinate of particles to spawn
+	 *            X coordinate of particles to spawn
 	 * @param Y
-	 *            - Y coordinate of particles to spawn
+	 *            Y coordinate of particles to spawn
 	 * @param Z
-	 *            - Z coordinate of particles to spawn
+	 *            Z coordinate of particles to spawn
 	 * @param particleType
-	 *            - The type of particle to spawn.
+	 *            The type of particle to spawn.
 	 * @param count
-	 *            - The (approximate?) numer of particles to create.
+	 *            The (approximate?) number of particles to create.
 	 */
 	public static void particles(World worldIn, double X, double Y, double Z, EnumParticleTypes particleType,
 			int count) {
