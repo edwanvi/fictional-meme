397daeab08a569260db74af7a4ccf5300116ec11
Edwan Vi <tkdberger@gmail.com>
2016-12-22 16:49:24 -0500

    S T A C K U N D E R F L O W

--- src/main/java/me/itstheholyblack/testmodpleaseignore/client/TileEntitySpellweaverRenderer.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/client/TileEntitySpellweaverRenderer.java
@@ -1,12 +1,20 @@
 package me.itstheholyblack.testmodpleaseignore.client;
 
 import me.itstheholyblack.testmodpleaseignore.blocks.tile_entities.TileEntitySpellweaver;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBook;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.items.CapabilityItemHandler;
 
 // @SideOnly(Side.CLIENT)
 public class TileEntitySpellweaverRenderer extends TileEntitySpecialRenderer<TileEntitySpellweaver> {
@@ -14,7 +22,13 @@ public class TileEntitySpellweaverRenderer extends TileEntitySpecialRenderer<Til
 	private static final ResourceLocation TEXTURE_BOOK = new ResourceLocation(
 			"textures/entity/enchanting_table_book.png");
 	private final ModelBook modelBook = new ModelBook();
+	private final RenderItem itemRenderer;
 
+	public TileEntitySpellweaverRenderer() {
+		super();
+		this.itemRenderer = Minecraft.getMinecraft().getRenderItem();
+	}
+	
 	@Override
 	public void renderTileEntityAt(TileEntitySpellweaver te, double x, double y, double z, float partialTicks,
 			int destroyStage) {
@@ -61,5 +75,35 @@ public class TileEntitySpellweaverRenderer extends TileEntitySpecialRenderer<Til
 		GlStateManager.enableCull();
 		this.modelBook.render((Entity) null, f, f3, f4, f5, 0.0F, 0.0625F);
 		GlStateManager.popMatrix();
+		this.renderItem(te, x, y, z, partialTicks, destroyStage);
+		GlStateManager.popMatrix();
+	}
+	private void renderItem(TileEntitySpellweaver te, double x, double y, double z, float partialTicks, int destroyStage) {
+		ItemStack itemstack = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).getStackInSlot(0);
+		if (!itemstack.isEmpty())
+        {
+            EntityItem entityitem = new EntityItem(te.getWorld(), 0.0D, 0.0D, 0.0D, itemstack);
+            Item item = entityitem.getEntityItem().getItem();
+            entityitem.getEntityItem().setCount(1);
+            entityitem.hoverStart = 0.0F;
+            GlStateManager.pushMatrix();
+            GlStateManager.disableLighting();
+            int i = (int) (te.bookRotation * -1);
+
+            if (item instanceof net.minecraft.item.ItemMap)
+            {
+                i = i % 4 * 2;
+            }
+
+            GlStateManager.rotate((float)i * 360.0F / 8.0F, 0.0F, 0.0F, 1.0F);
+
+            // net.minecraftforge.client.event.RenderItemInFrameEvent event = new net.minecraftforge.client.event.RenderItemInFrameEvent(te, this);
+            GlStateManager.scale(0.5F, 0.5F, 0.5F);
+            GlStateManager.pushAttrib();
+            RenderHelper.enableStandardItemLighting();
+            this.itemRenderer.renderItem(entityitem.getEntityItem(), ItemCameraTransforms.TransformType.FIXED);
+            RenderHelper.disableStandardItemLighting();
+            GlStateManager.popAttrib();
+        }
 	}
 }
