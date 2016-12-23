44ff77b609eb38aad30d73505fc4d471a7f23be6
Edwan Vi <tkdberger@gmail.com>
2016-12-23 13:43:02 -0500

    Oooh, aaah

--- src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/tile_entities/TileEntitySpellweaver.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/tile_entities/TileEntitySpellweaver.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import javax.annotation.Nullable;
 
+import me.itstheholyblack.testmodpleaseignore.core.ParticleEffects;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
@@ -12,6 +13,7 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -135,6 +137,7 @@ public class TileEntitySpellweaver extends TileEntity implements ITickable {
 	public void wrathOfGod() {
 		BlockPos pos = this.getPos();
 		world.addWeatherEffect(new EntityLightningBolt(world, pos.getX(), pos.getY(), pos.getZ(), false));
+		ParticleEffects.particles(world, pos.getX(), pos.getY(), pos.getZ(), EnumParticleTypes.LAVA, 100);
 	}
 
 	// Data syncing
