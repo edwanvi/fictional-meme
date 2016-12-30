0b3275b00aca76cf5c1199932c20d719d85b8864
Edwan Vi <tkdberger@gmail.com>
2016-12-30 12:42:10 -0500

    There is no new chapter

--- src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/tile_entities/AcceleratorTileEntity.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/tile_entities/AcceleratorTileEntity.java
@@ -3,20 +3,24 @@ package me.itstheholyblack.testmodpleaseignore.blocks.tile_entities;
 import me.itstheholyblack.testmodpleaseignore.blocks.ModBlocks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class AcceleratorTileEntity extends TileEntity implements ITickable {
 	private int count;
+	private double mana;
 	private boolean safe;
 	private BlockPos pos = getPos();
 
 	@Override
 	public void update() {
-		// safe = test_all_faces();
-		// if (worldObj.isRemote) {
 		if (count > 100 && !safe) {
 			this.world.newExplosion(null, getPos().getX(), getPos().getY(), getPos().getZ(), 4.0F, true, true);
 			markDirty();
@@ -25,7 +29,10 @@ public class AcceleratorTileEntity extends TileEntity implements ITickable {
 			if (!safe) {
 				increase();
 			} else {
-				// NO-OP
+				if (count < 0) {
+					decrease();
+				} // else no-op on count
+				mana = mana + 0.000001;
 			}
 			markDirty();
 		}
@@ -48,12 +55,14 @@ public class AcceleratorTileEntity extends TileEntity implements ITickable {
 	public void readFromNBT(NBTTagCompound compound) {
 		super.readFromNBT(compound);
 		count = compound.getInteger("counter");
+		mana = compound.getDouble("iveforgottenmyname");
 	}
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
 		super.writeToNBT(compound);
 		compound.setInteger("counter", count);
+		compound.setDouble("iveforgottenmyname", mana);
 		return compound;
 	}
 
@@ -64,5 +73,31 @@ public class AcceleratorTileEntity extends TileEntity implements ITickable {
 				return true;
 		}
 		return false;
+	}
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+		return oldState.getBlock() != newState.getBlock();
+	}
+
+	@Override
+	public final SPacketUpdateTileEntity getUpdatePacket() {
+		return new SPacketUpdateTileEntity(getPos(), -999, writeToNBT(new NBTTagCompound()));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public final void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+		super.onDataPacket(net, pkt);
+		readFromNBT(pkt.getNbtCompound());
+	}
+
+	@Override
+	public final NBTTagCompound getUpdateTag() {
+		return writeToNBT(new NBTTagCompound());
+	}
+
+	@Override
+	public final void handleUpdateTag(NBTTagCompound tag) {
+		readFromNBT(tag);
 	}
 }
