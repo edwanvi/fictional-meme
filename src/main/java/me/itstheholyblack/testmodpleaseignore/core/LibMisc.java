18d23776e671b8b1343b887dc4b723fe96b7ccb0
Edwan Vi <tkdberger@gmail.com>
2017-01-23 06:58:19 -0500

    Drawing spheres

--- src/main/java/me/itstheholyblack/testmodpleaseignore/core/LibMisc.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/core/LibMisc.java
@@ -1,9 +1,11 @@
 package me.itstheholyblack.testmodpleaseignore.core;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ChunkCache;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
 public class LibMisc {
@@ -12,6 +14,37 @@ public class LibMisc {
 			return ((ChunkCache) blockAccess).getTileEntity(pos, Chunk.EnumCreateEntityType.CHECK);
 		} else {
 			return blockAccess.getTileEntity(pos);
+		}
+	}
+
+	/**
+	 * Generate a sphere.
+	 * 
+	 * @author WayOfTime
+	 * @param world
+	 *            the world to put the sphere in
+	 * @param pos
+	 *            the position to draw the sphere at
+	 * @param materialBlock
+	 *            the block to build with
+	 */
+	public static void makeSphere(World world, BlockPos pos, IBlockState materialBlock, int radius) {
+		double floatingRadius = (double) radius;
+		for (int i = -radius; i <= radius; i++) {
+			for (int j = -radius; j <= radius; j++) {
+				for (int k = -radius; k <= radius; k++) {
+					if (i * i + j * j + k * k > (floatingRadius + 0.5) * (floatingRadius + 0.5)) {
+						continue;
+					}
+
+					BlockPos newPos = pos.add(i, j, k);
+					IBlockState state = world.getBlockState(newPos);
+
+					if (world.isAirBlock(newPos)) {
+						world.setBlockState(newPos, materialBlock);
+					}
+				}
+			}
 		}
 	}
 }
