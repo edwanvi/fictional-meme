3623229d663aca38d035a6b0b117dac415b2015f
Edwan Vi <tkdberger@gmail.com>
2017-02-16 17:35:16 -0500

    Man Fumes texture

--- src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/BlockPoisonGas.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/blocks/BlockPoisonGas.java
@@ -1,24 +1,35 @@
 package me.itstheholyblack.testmodpleaseignore.blocks;
 
+import java.util.Random;
+
 import javax.annotation.Nullable;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockBreakable;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockPoisonGas extends Block {
+public class BlockPoisonGas extends BlockBreakable {
 
 	public BlockPoisonGas() {
-		super(Material.AIR, MapColor.PURPLE);
+		super(Material.AIR, false, MapColor.PURPLE);
+		this.setTickRandomly(true);
 		setUnlocalizedName("manafumes");
 		setRegistryName("manafumes");
 		GameRegistry.register(this);
@@ -29,17 +40,20 @@ public class BlockPoisonGas extends Block {
 	public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid) {
 		return false;
 	}
-	
+
 	/**
-     * Called When an Entity Collided with the Block
-     */
+	 * Called When an Entity Collided with the Block
+	 */
 	@Override
-    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
-		if (!entityIn.isEntityInvulnerable(DamageSource.MAGIC)) {
-			entityIn.attackEntityFrom(DamageSource.MAGIC, 0.5F);
+	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+		
+		if (entityIn instanceof EntityLiving) {
+			if (!entityIn.isEntityInvulnerable(DamageSource.MAGIC)) {
+				entityIn.attackEntityFrom(DamageSource.MAGIC, 0.5F);
+			}
+			worldIn.setBlockToAir(pos);
 		}
-		worldIn.setBlockToAir(pos);
-    }
+	}
 
 	@Nullable
 	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
@@ -48,5 +62,31 @@ public class BlockPoisonGas extends Block {
 
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
 		return FULL_BLOCK_AABB;
+	}
+
+	// render methods
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
+	}
+
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void initModel() {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0,
+				new ModelResourceLocation(getRegistryName(), "inventory"));
+	}
+
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+		if (worldIn.getLightFor(EnumSkyBlock.BLOCK, pos) > 11 && rand.nextBoolean()) {
+			worldIn.setBlockToAir(pos);
+		}
 	}
 }
