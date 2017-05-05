6a3a84c21647e543eceead668433a0119932ec83
Edwan Vi <tkdberger@gmail.com>
2017-05-05 16:33:39 -0400

    Guess Eclipse like refactors lol

--- src/main/java/me/itstheholyblack/testmodpleaseignore/core/Randomizer.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/core/Randomizer.java
@@ -13,13 +13,13 @@ public class Randomizer {
 	/**
 	 * Generate a random boolean, with a bias {@code p} for being {@code true}.
 	 * 
-	 * @param p
+	 * @param d
 	 *            The chance of returning {@code true}. 0.5 = 50%.
 	 * @return A boolean
 	 * @author maxf130
 	 * @author Edwan Vi
 	 */
-	public static boolean getRandomBoolean(float p) {
-		return rand_gen.nextFloat() < p;
+	public static boolean getRandomBoolean(double d) {
+		return rand_gen.nextDouble() < d;
 	}
 }
