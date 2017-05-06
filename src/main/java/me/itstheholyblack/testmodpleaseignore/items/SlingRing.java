1925c890d54fc7cd07623a1ceee44c239ab69002
Edwan Vi <tkdberger@gmail.com>
2017-05-06 10:46:36 -0400

    No escaping the End

--- src/main/java/me/itstheholyblack/testmodpleaseignore/items/SlingRing.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/items/SlingRing.java
@@ -7,13 +7,16 @@ import me.itstheholyblack.testmodpleaseignore.core.CustomTeleporter;
 import me.itstheholyblack.testmodpleaseignore.core.PlayerDataMan;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Biomes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
@@ -21,7 +24,9 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProviderEnd;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.end.DragonFightManager;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -46,7 +51,6 @@ public class SlingRing extends Item {
 		if (stack.getTagCompound() == null || playerIn.isSneaking()) {
 			stack.setTagCompound(new NBTTagCompound());
 			NBTInit(stack, playerIn);
-			System.out.println(playerIn.getName() + " initialized a sling ring. \\[T]/");
 			if (!worldIn.isRemote) {
 				playerIn.sendStatusMessage(new TextComponentString(TextFormatting.GREEN + "Sling Ring initialized."),
 						false);
@@ -54,16 +58,39 @@ public class SlingRing extends Item {
 		} else {
 			NBTTagCompound compound = stack.getTagCompound();
 			particles(worldIn, playerIn);
-			if (playerIn instanceof EntityPlayerMP
-					&& compound.getInteger("dim") != playerIn.world.provider.getDimension()) {
-				CustomTeleporter.teleportToDimension(playerIn, compound.getInteger("dim"), compound.getInteger("x"),
-						compound.getInteger("y"), compound.getInteger("z"));
+			if (!worldIn.getBiome(playerIn.getPosition()).equals(Biomes.SKY)) {
+				if (playerIn instanceof EntityPlayerMP
+						&& compound.getInteger("dim") != playerIn.world.provider.getDimension()) {
+					CustomTeleporter.teleportToDimension(playerIn, compound.getInteger("dim"), compound.getInteger("x"),
+							compound.getInteger("y"), compound.getInteger("z"));
+				} else {
+					playerIn.setPositionAndUpdate(compound.getInteger("x"), compound.getInteger("y"),
+							compound.getInteger("z"));
+				}
+				particles(worldIn, playerIn);
+				PlayerDataMan.addMana(playerIn, -20.0D, true);
 			} else {
-				playerIn.setPositionAndUpdate(compound.getInteger("x"), compound.getInteger("y"),
-						compound.getInteger("z"));
+				if (worldIn.provider.getDimension() == 1) {
+					List<EntityDragon> list = worldIn.getEntities(EntityDragon.class, EntitySelectors.IS_ALIVE);
+					boolean dDead = false;
+					if (list.isEmpty()) {
+						dDead = true;
+					}
+					if (dDead) {
+						if (playerIn instanceof EntityPlayerMP
+								&& compound.getInteger("dim") != playerIn.world.provider.getDimension()) {
+							CustomTeleporter.teleportToDimension(playerIn, compound.getInteger("dim"),
+									compound.getInteger("x"), compound.getInteger("y"), compound.getInteger("z"));
+						} else {
+							playerIn.setPositionAndUpdate(compound.getInteger("x"), compound.getInteger("y"),
+									compound.getInteger("z"));
+						}
+						particles(worldIn, playerIn);
+						PlayerDataMan.addMana(playerIn, -20.0D, true);
+					}
+				}
 			}
-			particles(worldIn, playerIn);
-			PlayerDataMan.addMana(playerIn, -20.0D, true);
+
 			// ANTI-SPAGHETTI
 			if (playerIn.getGameProfile().getName().compareToIgnoreCase("AlexisMachina") == 0
 					|| playerIn.getGameProfile().getName().compareToIgnoreCase("Elucent") == 0
