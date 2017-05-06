50e98b97b39e27bd0fd7546fe87b6ffcabbcd0e4
Edwan Vi <tkdberger@gmail.com>
2017-05-06 10:46:24 -0400

    Headshot code

--- src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityExplosiveArrow.java
+++ src/main/java/me/itstheholyblack/testmodpleaseignore/entity/EntityExplosiveArrow.java
@@ -1,19 +1,21 @@
 package me.itstheholyblack.testmodpleaseignore.entity;
 
 import me.itstheholyblack.testmodpleaseignore.core.Randomizer;
+import me.itstheholyblack.testmodpleaseignore.items.ModItems;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Biomes;
 import net.minecraft.init.PotionTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class EntityExplosiveArrow extends EntityArrow {
 
 	@Override
 	protected ItemStack getArrowStack() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ItemStack(ModItems.eArrow);
 	}
 
 	public EntityExplosiveArrow(World worldIn) {
@@ -32,7 +34,14 @@ public class EntityExplosiveArrow extends EntityArrow {
 	protected void arrowHit(EntityLivingBase living) {
 		super.arrowHit(living);
 		BlockPos pos = living.getPosition();
+		Vec3d myPos = this.getPositionVector();
+		Vec3d eyes = living.getPositionEyes(1);
 		World world = living.world;
+		if (myPos.yCoord >= living.getPositionEyes(1).yCoord - 0.4
+				&& myPos.yCoord <= living.getPositionEyes(1).yCoord + 0.4) {
+			System.out.println("Succesful headshot!");
+			living.setDead();
+		}
 		if (!this.isBurning()) {
 			world.createExplosion(this, pos.getX(), pos.getY(), pos.getZ(), 3, false);
 		} else {
@@ -42,18 +51,22 @@ public class EntityExplosiveArrow extends EntityArrow {
 
 	public void onUpdate() {
 		super.onUpdate();
-		if (!this.world.isRemote && this.inGround) {
-			if (!this.isBurning()) {
-				world.createExplosion(this, this.posX, this.posY, this.posZ, 4.0F, true);
-			} else {
-				world.createExplosion(this, this.posX, this.posY, this.posZ, 8.0F, true);
+		if (!this.world.getBiome(this.getPosition()).equals(Biomes.SKY)) {
+			if (!this.world.isRemote && this.inGround) {
+				if (!this.isBurning()) {
+					world.createExplosion(this, this.posX, this.posY, this.posZ, 4.0F, true);
+				} else {
+					world.createExplosion(this, this.posX, this.posY, this.posZ, 8.0F, true);
+				}
+				this.setDead();
 			}
-			this.setDead();
+			if (this.isBurning() && !this.isDead && Randomizer.getRandomBoolean(0.25)
+					&& (this.ticksExisted % 10 == 0)) {
+				System.out.println("midair explosions!");
+				world.createExplosion(this, this.posX, this.posY, this.posZ, 8.0F, true);
+				this.setDead();
+			}
 		}
-		if (this.isBurning() && !this.isDead && Randomizer.getRandomBoolean(0.25) && (this.ticksExisted % 10 == 0)) {
-			System.out.println("midair explosions!");
-			world.createExplosion(this, this.posX, this.posY, this.posZ, 8.0F, true);
-			this.setDead();
-		}
+
 	}
 }
